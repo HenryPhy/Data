4dcd4d4ca209ffe2a370b7678a8b516eb5748cea
Henry <15810205208@163.com>
2018-07-05 09:14:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/data/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.data;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void date(View view) {
+        //获取日期
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, year + "年" + (month + year) + "月" + dayOfMonth + "日", Toast.LENGTH_LONG).show();
+
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+
+    }
+
+    public void time(View view) {
+        //获取时间
+        Calendar calendar = Calendar.getInstance();
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this,hourOfDay+":"+minute,Toast.LENGTH_LONG).show();
+
+            }
+        },calendar.get(Calendar.HOUR),calendar.get(Calendar.MINUTE),true);
+        timePickerDialog.show();
+    }
+}
